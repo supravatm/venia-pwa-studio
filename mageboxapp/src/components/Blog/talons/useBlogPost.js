835b2ee9aae2e4a6008e021906734938b3997364
Supravat Mondal <6244832+supravatm@users.noreply.github.com>
2026-07-01 13:22:46 +0530

    block optimize

--- mageboxapp/src/components/Blog/talons/useBlogPost.js
+++ mageboxapp/src/components/Blog/talons/useBlogPost.js
@@ -1,57 +1,22 @@
 import { useEffect, useMemo, useState } from 'react';
-
+import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
-import { useAwaitQuery } from '@magento/peregrine/lib/hooks/useAwaitQuery';
-
 import { GET_BLOG_POST } from './blog.gql';
 
 export const useBlogPost = () => {
     const { urlKey } = useParams();
 
-    const fetchBlogPost = useAwaitQuery(GET_BLOG_POST);
-
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [post, setPost] = useState(null);
-
-    useEffect(() => {
-        let mounted = true;
-
-        const loadPost = async () => {
-            try {
-                const { data } = await fetchBlogPost({
-                    variables: {
-                        urlKey
-                    }
-                });
-
-                if (mounted) {
-                    setPost(data.blogPostByUrlKey);
-                }
-            } catch (err) {
-                if (mounted) {
-                    setError(err);
-                }
-            } finally {
-                if (mounted) {
-                    setLoading(false);
-                }
-            }
-        };
-
-        loadPost();
-
-        return () => {
-            mounted = false;
-        };
-    }, [fetchBlogPost, urlKey]);
+    const { data, loading, error } = useQuery(GET_BLOG_POST, {
+        variables: { urlKey },
+        fetchPolicy: 'cache-and-network'
+    });
 
     return useMemo(
         () => ({
             loading,
             error,
-            post
+            post: data?.blogPostByUrlKey || null
         }),
-        [loading, error, post]
+        [loading, error, data]
     );
 };
